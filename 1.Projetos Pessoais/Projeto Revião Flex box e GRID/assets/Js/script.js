40ca80117dd4a81f63b06e1cbf863932b1a10132
Leonardo Pereira Vaz <leonardo72011@live.com>
2023-01-09 10:47:13 -0300

    Revisão atualizada

--- 1.Projetos Pessoais/Projeto Revião Flex box e GRID/assets/Js/script.js
+++ 1.Projetos Pessoais/Projeto Revião Flex box e GRID/assets/Js/script.js
@@ -1,1 +1,15 @@
-console.log("ok")
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+    proximaImg()
+}, 2000);
+
+function proximaImg() {
+    count++;
+    if (count > 5) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
